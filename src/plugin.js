dee9113550945e4a4a4f9551454305f743315f6a
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2020-01-07 13:37:21 +0900

    create basic plugin

---
+++ src/plugin.js
@@ -0,0 +1,16 @@
+const plugin = ({types: t}) => {
+  return {
+    visitor: {
+      BinaryExpression(path) {
+        if (path.node.operator !== "===") {
+          return;
+        }
+
+        path.node.left = t.identifier("sebmck");
+        path.node.right = t.identifier("dork");
+      }
+    }
+  }
+}
+
+module.exports = plugin
