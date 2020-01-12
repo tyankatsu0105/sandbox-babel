db6285e2bd7fc6684bec06a63126c67ca97e737c
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-01-12 17:48:31 +0900

    update package

--- config/jest/custom-transformer.js
+++ config/jest/custom-transformer.js
@@ -2,15 +2,7 @@ const babelJest = require("babel-jest");
 
 /** @type import('babel__core').TransformOptions */
 const transformOptions = {
-    presets: [
-        ["@babel/env", {
-            "targets": {
-                "node": "current",
-            },
-            "exclude": ["transform-regenerator"],
-        }],
-    ],
-    babelrc: false,
+    "plugins": ["@babel/plugin-transform-modules-commonjs"]
 };
 
 module.exports = babelJest.createTransformer(transformOptions);
