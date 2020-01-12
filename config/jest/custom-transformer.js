3236d4f44c7dd4cd3f3208397086baf504a7a925
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-01-12 18:01:58 +0900

    chore

--- config/jest/custom-transformer.js
+++ config/jest/custom-transformer.js
@@ -2,7 +2,7 @@ const babelJest = require("babel-jest");
 
 /** @type import('babel__core').TransformOptions */
 const transformOptions = {
-    "plugins": ["@babel/plugin-transform-modules-commonjs"]
+    plugins: ["@babel/plugin-transform-modules-commonjs"]
 };
 
 module.exports = babelJest.createTransformer(transformOptions);
